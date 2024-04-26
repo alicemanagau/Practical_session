a980586b9ca6de12499b658e1f0860b7534461c5
Alice <alice.managau@gmail.com>
2024-04-26 15:33:34 +0200

    Round robin

---
+++ roundrobin.c
@@ -0,0 +1,28 @@
+#include <string>
+#include <stdio.h>
+
+int task1()
+{
+	printf("Le premier processus est executé");
+	return 0;	
+
+}
+
+int task2()
+{
+        printf("Le deuxieme processus est executé");
+        return 0;
+
+}
+
+int task3()
+{
+        printf("Le troisieme processus est executé");
+        return 0;
+
+}
+
+void main()
+{
+
+}
